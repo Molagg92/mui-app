1d1b6b4d4056d07433c7a6778d9fa1bbc7576689
Erik Z <molagg92@gmail.com>
2024-01-24 16:42:31 -0800

    added my first text in mui format

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@mui/material';
+import { PhotoCamera } from '@material-ui/icons';
+
+const App = () => {
+    return (
+      <>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <PhotoCamera />
+            <Typography variant="h6">
+                Photo Album
+            </Typography>
+      
+          </Toolbar>
+        </AppBar>
+      </>
+    );
+}
+
+export default App;
